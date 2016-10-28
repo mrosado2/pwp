118b43e5d2e04ad48e8588944b39d4b8414159ce
Maria Rosado <mrosado2@cnm.edu>
2016-10-27 22:56:59 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	public_html

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+
+			<meta charset="UTF-8"/>
+
+			<!-- title of assignment here -->
+
+			<title>Milestome 2</title>
+		</head>
+		<body>
+			<header>
+				<h1>My Personal Webpage</h1>
+				<h2>Maria Rosado</h2>
+
+			</header>
+			<main>
+				<h2>Site Content</h2>
+				<p>The content of this webpage will be a number of projects that I have worked individually and the ones
+				that I have contributed </p>
+				<ul>
+					<li>Header:</li>
+					<li>About Me:</li>
+					<li>Portfolio:</li>
+					<li>Contact:</li>
+
+				</ul>
+
+					<!-- wireframes -->
+				<section>
+					<h2> Full Screen</h2>
+					<img src="">
+
+					<h2>Mobile</h2>
+					<img src="">
+
+				</section>
+			</main>
+		</body>
+</html>
