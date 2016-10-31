ea13e9c5390431ad0dfb1eb5b5b0f383c599f6ba
Maria Rosado <mrosado2@cnm.edu>
2016-10-30 23:23:58 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	public_html

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,27 +12,29 @@
 			<header>
 				<h1>My Personal Webpage</h1>
 				<h2>Maria Rosado</h2>
-
 			</header>
+			<!-- Content ans strategy -->
 			<main>
 				<h2>Site Content</h2>
-				<p>The content of this webpage will be a number of projects that I have worked individually and the ones
-				that I have contributed </p>
+				<p>The content of this page, will be some of the projects that I have worked individually and the ones
+				that I have contributed to.</p>
 				<ul>
-					<li>Header:</li>
-					<li>About Me:</li>
-					<li>Portfolio:</li>
-					<li>Contact:</li>
-
+					<li><strong>Header:</strong> This page will include a large background with my picture and my name at
+						the center, a large background with my picture and small description of why they
+						should hire me.</li>
+					<li><strong>About Me:</strong> I will describe my experience, skills and will mention my favorite project.</li>
+					<li><strong>Portfolio:</strong> Here I will show projects that will show variety of my skills.</li>
+					<li><strong>Contact:</strong> This page will have a form where they may contact me giving me general
+					details of the type of project they want to develop.</li>
 				</ul>
 
 					<!-- wireframes -->
 				<section>
-					<h2> Full Screen</h2>
-					<img src="">
+					<h2>Desktop</h2>
+					<img src="../img/maria-fullscreen.jpg" alt="fullscreen wireframe ">
 
 					<h2>Mobile</h2>
-					<img src="">
+					<img src="../img/maria-mobile.jpg" alt="mobile wireframe " >
 
 				</section>
 			</main>
