c45e088ccc87487eeb525e736a90cca18ba3fdd6
Maria Rosado <mrosado2@cnm.edu>
2016-11-21 01:20:15 -0700

    added some images

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,20 +6,20 @@
 
 			<!-- title of assignment here -->
 
-			<title>Milestome 2</title>
+			<title>Milestone 2</title>
 		</head>
 		<body>
 			<header>
-				<h1>My Personal Webpage</h1>
+				<h1>Milestone 2</h1>
+				<h2>My Personal Webpage</h2>
 				<h2>Maria Rosado</h2>
 			</header>
 			<!-- Content ans strategy -->
-			<main>
 				<h2>Site Content</h2>
 				<p>The content on this page will be some of the projects that I have worked individually and the ones
 				that I have contributed to.</p>
 				<ul>
-					<li><strong>Header:</strong> This page will include a large background with my picture and my name at
+					<li><strong>Home:</strong> This page will include a large background with my picture and my name at
 						the center, and small description of why they should hire me.</li>
 					<li><strong>About Me:</strong> I will describe my experience, skills and will mention my favorite project.</li>
 					<li><strong>Portfolio:</strong> Here I will show projects that will show variety of my skills.</li>
@@ -36,6 +36,5 @@
 					<img src="../img/maria-mobile.jpg" alt="mobile wireframe " >
 
 				</section>
-			</main>
 		</body>
 </html>
---
+++ public_html/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<meta charset="UTF-8"/>
+		<meta http-equiv=""X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- ADD YOUR CUSTOM CSS HERE -->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+		<script src="js/script.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!--FONT AWESOME-->
+		<script src="https://use.fontawesome.com/ccafd2629e.js"></script>
+
+		<!--Google Fonts-->
+		<link
+			href="https://fonts.googleapis.com/css?family=Abril+Fatface|Cardo|Cormorant+Infant|Crimson+Text|Josefin+Sans|Poiret+One|Roboto+Mono|Ubuntu+Condensed"
+			rel="stylesheet">
+
+		<!-- title of assignment here -->
+
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<header>
+			<h1>Milestone 2</h1>
+			<h2>My Personal Webpage</h2>
+			<h2>Maria Rosado</h2>
+		</header>
+		<!-- Content ans strategy -->
+		<h2>Site Content</h2>
+		<p>The content on this page will be some of the projects that I have worked individually and the ones
+			that I have contributed to.</p>
+		<ul>
+			<li><strong>Home:</strong> This page will include a large background with my picture and my name at
+				the center, and small description of why they should hire me.</li>
+			<li><strong>About Me:</strong> I will describe my experience, skills and will mention my favorite project.</li>
+			<li><strong>Portfolio:</strong> Here I will show projects that will show variety of my skills.</li>
+			<li><strong>Contact:</strong> This page will have a form where they may contact me giving me general
+				details of the type of project they want to develop.</li>
+		</ul>
+
+		<!-- wireframes -->
+		<section>
+			<h2>Desktop</h2>
+			<img src="../img/maria-fullscreen.jpg" alt="fullscreen wireframe ">
+
+			<h2>Mobile</h2>
+			<img src="../img/maria-mobile.jpg" alt="mobile wireframe " >
+
+		</section>
+	</body>
+</html>
