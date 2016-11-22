e9b8f02c53e816558ed8b1b9509733fb1f5408eb
Maria Rosado <mrosado2@cnm.edu>
2016-11-22 00:59:03 -0700

    added some images

--- public_html/index.php
+++ public_html/index.php
@@ -1,59 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
-
-		<meta charset="UTF-8"/>
-		<meta http-equiv=""X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- ADD YOUR CUSTOM CSS HERE -->
-		<link rel="stylesheet" href="css/style.css" type="text/css"/>
-
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-
-		<!-- jQuery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-
-		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript"
-				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript"
-				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
-		<script type="text/javascript"
-				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
-
-		<!-- Your JavaScript Form Validator -->
-		<script src="js/form-validate.js"></script>
-		<script src="js/script.js"></script>
-
-		<!-- Google reCAPTCHA -->
-		<script src='https://www.google.com/recaptcha/api.js'></script>
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
-
-		<!--FONT AWESOME-->
-		<script src="https://use.fontawesome.com/ccafd2629e.js"></script>
-
-		<!--Google Fonts-->
-		<link
-			href="https://fonts.googleapis.com/css?family=Abril+Fatface|Cardo|Cormorant+Infant|Crimson+Text|Josefin+Sans|Poiret+One|Roboto+Mono|Ubuntu+Condensed"
-			rel="stylesheet">
-
-		<!-- title of assignment here -->
-
-		<title>Milestone 2</title>
+			<meta charset="UTF-8"/>
+			<?php require_once("../lib/head-utils.php");?>
 	</head>
+
+	<!--begin header and navbar-->
 	<body>
 		<header>
 			<h1>Milestone 2</h1>
---
+++ public_html/lib/head.utils.php
@@ -0,0 +1,47 @@
+<meta charset="UTF-8"/>
+<meta http-equiv=""X-UA-Compatible" content="IE=edge"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- ADD YOUR CUSTOM CSS HERE -->
+<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+<!-- jQuery Form, Additional Methods, Validate -->
+<script type="text/javascript"
+		  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+<script type="text/javascript"
+		  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+<script type="text/javascript"
+		  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+<!-- Your JavaScript Form Validator -->
+<script src="js/form-validate.js"></script>
+<script src="js/script.js"></script>
+
+<!-- Google reCAPTCHA -->
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+		  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+		  crossorigin="anonymous"></script>
+
+<!--FONT AWESOME-->
+<script src="https://use.fontawesome.com/ccafd2629e.js"></script>
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+<title>PWP</title>
