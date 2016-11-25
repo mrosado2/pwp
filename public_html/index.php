97a234040d5577ba46ed0a45905a4bb72394fd37
Maria Rosado <mrosado2@cnm.edu>
2016-11-25 10:45:56 -0700

    renamed  and deleted some files

--- public_html/index.php
+++ public_html/index.php
@@ -9,8 +9,10 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-		<!-- ADD YOUR CUSTOM CSS HERE -->
-		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+		<!-- ADD YOUR CUSTOM <!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		href="css/style.css" type="text/css"/>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -65,26 +67,26 @@
 				</div>
 			</div>
 		</section>
-					<!--Toggle and brand for better mobile display-->
-					<div class=navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data toggle="collapse"
-								  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand page-scroll">| BlackCat Web Developer |</a>
-					</div>
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav navbar-left">
-							<li><a class="page-scroll" href="#home">Home</a></li>
-							<li><a class="page-scroll" href="#about">About Me</a></li>
-							<li><a class="page-scroll" href="#portfolio">Portfolio</a></li>
-						</ul>
-					</div>
-			</nav>
+		<!--Toggle and brand for better mobile display-->
+		<div class=navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data toggle="collapse"
+					  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand page-scroll">| BlackCat Web Developer |</a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-left">
+				<li><a class="page-scroll" href="#home">Home</a></li>
+				<li><a class="page-scroll" href="#about">About Me</a></li>
+				<li><a class="page-scroll" href="#portfolio">Portfolio</a></li>
+			</ul>
+		</div>
+		</nav>
 
 		<!--Main Page Content  -->
 		<main>
@@ -100,27 +102,27 @@
 			</div>-->
 			<!-- About Me -->
 			<id="about" class="container about section">
-				<div><h2>About Me</h2></div>
-				<div class="row">
-					<div class="col-md-6">
-						<p>Hello, I'm Maria and I am a Web Developer. I pretty much enjoy solving problems. I have done so in all
-							my jobs.</p>
-						<p>I have experience in sales, manufacturing, training and events organization.</p>
-						<p>I have helped my employers save money and time by implementing technology and creative solutions.</p>
-						<p>If you would like to find our where you may find opportunities to increase your productivity and
-							cut costs, contact me.</p>
-					</div>
-					<div class="col-md-6">
-						<img class="maria-profile" src="img/maria-profile.png" alt="maria-profile">
-					</div>
+			<div><h2>About Me</h2></div>
+			<div class="row">
+				<div class="col-md-6">
+					<p>Hello, I'm Maria and I am a Web Developer. I pretty much enjoy solving problems. I have done so in all
+						my jobs.</p>
+					<p>I have experience in sales, manufacturing, training and events organization.</p>
+					<p>I have helped my employers save money and time by implementing technology and creative solutions.</p>
+					<p>If you would like to find our where you may find opportunities to increase your productivity and
+						cut costs, contact me.</p>
+				</div>
+				<div class="col-md-6">
+					<img class="maria-profile" src="img/maria-profile.png" alt="maria-profile">
+				</div>
 		</main>
 		<!-- Portfolio -->
 		</form>
-					</div>
-					<div class="col-xs-12 col-md-6 portfolio-caption">
-						<h4>Growify</h4>
-						<p class="text-muted">Description</p>
-					</div>
+		</div>
+		<div class="col-xs-12 col-md-6 portfolio-caption">
+			<h4>Growify</h4>
+			<p class="text-muted">Description</p>
+		</div>
 
 		<!-- contact section - need to learn how to create contact form -->
 
--- public_html/lib/head.utils.php
+++ public_html/lib/head.utils.php
@@ -5,6 +5,9 @@
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+		integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 <!-- ADD YOUR CUSTOM CSS HERE -->
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
