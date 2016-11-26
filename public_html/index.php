9652f04c04eb2b83f279d1e4f0ab6bb406dea7f1
Maria Rosado <mrosado2@cnm.edu>
2016-11-26 01:00:04 -0700

    added twitter and in acct and added some files

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,60 @@
 <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/html">
 	<head>
-		<?php require_once("..public_html/lib/head_utils.php");?>
+		<meta charset="UTF-8"/>
+		<meta http-equiv=""X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- ADD YOUR CUSTOM CSS HERE -->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<!-- my custom css file -->
+		<link rel="stylesheet" href="../public_html/css/style.css" type="text/css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+		<script src="js/script.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!--FONT AWESOME-->
+		<script src="https://use.fontawesome.com/ccafd2629e.js"></script>
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+
+		<title>BlackCat Web developer</title>
 		<title>My Personal Website</title>
 	</head>
 	<!-- Body Container-->
@@ -95,7 +147,7 @@
 				<h2 class="heading">Portfolio</h2>
 				<div class="row"> <!--row flex is not working here; so I didn't include it-->
 					<div class="col-md-3">
-						<img class="img-responsive portfolio-img" src="images/LogoMakr.png" alt="flek logo">
+						<img class="img-responsive portfolio-img" src="images/code image.png" alt="code image">
 						<div class="caption">
 							<h3>
 								Flek
@@ -108,7 +160,7 @@
 							<a href="//flek.me" target="_blank"
 								class="btn btn-primary" role="button"><i
 									class="fa fa-lg fa-external-link"></i></a>
-							<a href="//github.com/gsandoval49/flek" target="_blank"
+							<a href="//github.com/kdilts/Aaaa-capstoe" target="_blank"
 								class="btn btn-default pull-right"
 								role="button"><i class="fa fa-lg fa-github"></i></a>
 						</div>
@@ -146,7 +198,7 @@
 							<a href="//google.com/" target="_blank"
 								class="btn btn-primary" role="button"><i
 									class="fa fa-lg fa-external-link"></i></a>
-							<a href="//github.com/2" target="_blank"
+							<a href="//github.com/mrosado2" target="_blank"
 								class="btn btn-default pull-right"
 								role="button"><i class="fa fa-lg fa-github"></i></a>
 						</div>
@@ -165,7 +217,7 @@
 							<a href="//google.com/" target="_blank"
 								class="btn btn-primary" role="button"><i
 									class="fa fa-lg fa-external-link"></i></a>
-							<a href="//github.com/csosa2" target="_blank"
+							<a href="//github.com/mrosado2" target="_blank"
 								class="btn btn-default pull-right"
 								role="button"><i class="fa fa-lg fa-github"></i></a>
 						</div>
@@ -254,7 +306,7 @@
 							<span class="fa fa-github fa-2x"></span>
 						</a>
 						<!--LINKEDIN ICON-->
-						<a href="//www.linkedin.com/in/maria-rosado-bb1608132" target="_blank"
+						<a href="//www.linkedin.com/in/maria-rosado" target="_blank"
 							class="btn btn-social-icon btn-linkedin">
 							<span class="fa fa-linkedin fa-2x"></span>
 						</a>
