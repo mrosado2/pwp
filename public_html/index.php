413c6aaeb09c395d889040eb8515bde2449386d2
Maria Rosado <mrosado2@cnm.edu>
2016-12-03 23:23:35 -0700

    corrected css styles.css

--- public_html/index.php
+++ public_html/index.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
+<html lang="en">
 	<head>
+
 		<meta charset="UTF-8"/>
 		<meta http-equiv=""X-UA-Compatible" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
 				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
 		<!-- ADD YOUR CUSTOM CSS HERE -->
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
-
 		<!-- my custom css file -->
 		<link rel="stylesheet" href="../public_html/css/style.css" type="text/css">
-
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
 		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 		<![endif]-->
-
 		<!-- jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-
 		<!-- jQuery Form, Additional Methods, Validate -->
 		<script type="text/javascript"
 				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
@@ -35,185 +34,162 @@
 				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
 		<script type="text/javascript"
 				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
-
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
 		<script src="js/script.js"></script>
-
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
-
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 				  crossorigin="anonymous"></script>
-
 		<!--FONT AWESOME-->
 		<script src="https://use.fontawesome.com/ccafd2629e.js"></script>
-
 		<!-- Google Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
 
-		<title>BlackCat Web developer</title>
+		<title>BlackCat Web development</title>
 		<title>My Personal Website</title>
 	</head>
-	<!-- Body Container-->
-	<body class="footer">
-
-		<!begin sticky footer content -->
+	<body class="sfooter">
 		<div class="sfooter-content">
+			<header>
 
-			<!------------- Header ------------->
+				<!-- begin header and navbar-->
+				<nav class="nav-scroll navbar navbar-default navbar-fixed-top">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span> class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand page-scroll">| BlackCat Web Develoment |</a>
+						</div>
 
-		<header>
-			<!------- Begin Navbar ------->
-			<nav class="nav-scroll navbar navbar-default navbar-fixed-top">
-				<div class="container-fluid">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-								  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span> class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand page-scroll">| Black Cat Develoment |</a>
-					</div>
-
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav navbar-left">
 								<li><a class="page-scroll" href="#home">Home</a></li>
 								<li><a class="page-scroll" href="#about">About Me</a></li>
 								<li><a class="page-scroll" href="#portfolio">Portfolio</a></li>
 								<li><a class="page-scroll" href="#contact">Contact Me</a> </li>
 							</ul>
+						</div>
+					</div>
+				</nav>
+
+			</header>
+
+			<!-- begin welcome section -->
+			<section>
+				<div class="container">
+					<img src="..public_html/img/class="background"
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="jumbotron text-center">
+								<h1>Maria Rosado</h1>
+								<h2> Web Developer</h2>
+								<p class="lead"></p>
+							</div>
+						</div><!-- /.col-sm-12 -->
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+			</section>
+
+			<!-- begin photo/text section -->
+			<a name="about"></a><!-- this is an "anchor" link -->
+			<section class="bg-gray">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-3">
+							<img src="../public_html/img/maria-profile.png"
+								  class="img-responsive img-thumbnail xs-center-block m-b-2">
+						</div>
+						<div class="col-sm-9">
+							<div class="About me"></div><h2>About Me</h2>
+							<p class="lead">Hi! My name is Maria Rosado. I am a Web Developer. Photography aficionado, with a love
+								for creating amazing Websites that look great and have seamless transitions from the eye catchers
+								to it effectiveness.
+							</p>
+							<p>Love to solve problems and help business to find was to use technology and get the most of it,
+								without loosing the human touch.</p>
+							<p>I am based in Albuquerque, New Mexico, and benefit of a multicultural background.
+								I was born in Mexico City where had an amazing job that send me to work in very importants events
+								like the first World Summit in Cancun and presidential campains.</p>
+							<p>Never missed the opportunity to visit museaums and historical locations from the Mayas in the south,
+								Aztecs ruins in central Mexico, to the Tarahumaras in the north.
+								Moved to New York where lived for 13 chilling winters and summers that were never enough to get to
+								visit all the stores and breath taking views on the Fall. There I worked with apparel companies
+								through an import, export, storage and transportation company where I help them to implement
+								technology they already had but were not using to full advantage.</p>
+							<p>Within the first month of my implementation of my suggested changes they save over $3,000.00 dollars.
+								That job also gave me the opportunity to work and learn from cultures from around the world.
+								Then moved to New Mexico to work at Intel. There I learned soo much about cutting edge technology
+								and manufacturing.
+								Now I am putting all that experience to the advantage of my customers.</p>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+			</section>
+
+			<!-- begin 4 column section -->
+
+			<a name="moar"></a><!-- this is an "anchor" link -->
+			<section>
+				<div class="container">
+					<div class="jumbotron text-center">
+						<div class="text-center m-b-2"></div><h2>Portfolio</h2></div>
+					<div class="text-Portfolio"
+					<div class="row">
+			</section>
+			<div class="col-sm-3">
+				<div class="text-center-m-b-2">
+					<img src="../public_html/img/icon-ddcb-lg.png" alt="ddcb" class="img-responsive img-circle img-thumbnail">
+					<h5>Aaaa-capstone</h5>
+					<p class="text-center m-b-2">Project</p>
+				</div>
+			</div><!-- /.col-sm-3 -->
+
+			<div class="col-sm-3">
+				<div class="text-center m-b-2">
+					<img src="../public_html/img/coming-soon-1.png" alt="coming-soon"
+						  class="img-responsive img-circle img-thumbnail">
+					<div>
+						<h3>Title 2</h3>
+						<p class="text-center m-b-2">Project</p>
 					</div>
 				</div>
-			</nav>
-		</header>
-			<!------------- Begin Main Content ------------->
-			<main>
-				<!--<div id="page-top" class="row">
-					<div class="col-xs-12">-->
-				<div id="home" class="jumbotron text-center">
-					<h1>Maria Rosado</h1>
-					<p>Web Developer</p>
-					<br>
-					<i class=""fa fa-angle-down fa 5x" aria-hidden="true"></i>
-				</div>
-			<!--</div>
-		</div>-->
+			</div><!-- /.col-sm-3 -->
 
-		<!------------- About Me ------------->
-		<div id="about" class="container about section"
-			  <h2 class="heading">About Me</h2>
-				<div class="row">
-					<div class="col-md-6">
-						<p>Hi, My name is Maria Rosado. I'm a web developer and my passion is to help my customers to find
-						opportunities to increase their productivity and business opportunities.
-						I formerly worked as a technician on manufacturing industry and that gave me experience of team work,
-						customer service, import and export.
-	</div>-->
-			<div class="col-md-6">
-					<img class="maria-profile" src="img/maria-profile.png" alt="maria-profile">
-				</div>
-		</div>
-	</div>
-
-		</main>
-		<!-- Portfolio -->
-		</form>
-		</div>
-		<div class="col-xs-12 col-md-6 portfolio-caption">
-			<h4>Growify</h4>
-			<p class="text-muted">Description</p>
-		</div>
-		<div id="portfolio" class="section">
-			<div class="container">
-				<h2 class="heading">Portfolio</h2>
-				<div class="row"> <!--row flex is not working here; so I didn't include it-->
-					<div class="col-md-3">
-						<img class="img-responsive portfolio-img" src="images/code image.png" alt="code image">
-						<div class="caption">
-							<h3>
-								Aaaa
-							</h3>
-							<p>
-								Connecting artists with patrons of local culture and art.
-							</p>
-						</div>
-						<div class="caption-padding">
-							<a href="//Aaaa-capstone" target="_blank"
-								class="btn btn-primary" role="button"><i class="fa fa-lg fa-external-link"></i></a>
-							<a href="//github.com/kdilts/Aaaa-capstoe" target="_blank"
-								class="btn btn-default pull-right"
-								role="button"><i class="fa fa-lg fa-github"></i></a>
-						</div>
-					</div>
-					<div class="col-md-3">
-						<img class="portfolio-img coming-soon" src="images/coming-soon.jpg" alt="coming-soon">
-						<div class="caption">
-							<h3>
-								Project
-							</h3>
-							<p>
-								Please check back soon. A fantastic webiste will be launching!
-							</p>
-						</div>
-						<div class="caption-padding">
-							<a href="//google.com/" target="_blank"
-								class="btn btn-primary" role="button"><i
-									class="fa fa-lg fa-external-link"></i></a>
-							<a href="//github.com/mrosado2" target="_blank"
-								class="btn btn-default pull-right"
-								role="button"><i class="fa fa-lg fa-github"></i></a>
-						</div>
-					</div>
-					<div class="col-md-3">
-						<img class="portfolio-img coming-soon" src="images/coming-soon.png" alt="coming-soon">
-						<div class="caption">
-							<h3>
-								Project
-							</h3>
-							<p>
-								Please check back soon. A fantastic webiste will be launching!
-							</p>
-						</div>
-						<div class="caption-padding">
-							<a href="//google.com/" target="_blank"
-								class="btn btn-primary" role="button"><i
-									class="fa fa-lg fa-external-link"></i></a>
-							<a href="//github.com/mrosado2" target="_blank"
-								class="btn btn-default pull-right"
-								role="button"><i class="fa fa-lg fa-github"></i></a>
-						</div>
-					</div>
-					<div class="col-md-3">
-						<img class="portfolio-img coming-soon" src="images/coming-soon.png" alt="coming soon">
-						<div class="caption">
-							<h3>
-								Project
-							</h3>
-							<p>
-								Please check back soon. A fantastic webiste will be launching!
-							</p>
-						</div>
-						<div class="caption-padding">
-							<a href="//google.com/" target="_blank"
-								class="btn btn-primary" role="button"><i
-									class="fa fa-lg fa-external-link"></i></a>
-							<a href="//github.com/mrosado2" target="_blank"
-								class="btn btn-default pull-right"
-								role="button"><i class="fa fa-lg fa-github"></i></a>
-						</div>
+			<div class="col-sm-3">
+				<div class="text-center m-b-2">
+					<img src="../public_html/img/coming-soon-1.png" alt="coming-soon"
+						  class="img-responsive img-circle img-thumbnail">
+					<div>
+						<h3>Title 3</h3>
+						<p class="text-center m-b-2">Project</p>
 					</div>
 				</div>
-			</div>
-		</div>
+			</div><!-- /.col-sm-3 -->
+			<div class="col-sm-3">
+				<div class="text-center m-b-2">
+					<img src="../public_html/img/coming-soon-1.png" alt="coming-soom"
+						  class="img-responsive img-circle img-thumbnail">
+					<div>
+						<h3>Title 4</h3>
+						<p class="text-center m-b-2">Project</p>
+					</div>
+				</div>
+			</div><!-- /.col-sm-3 -->
 
-
+		</div><!-- /.row -->
+		</div><!-- /.container -->
+		</section>
+		</div><!-- /.sfooter-content -->
 		<!------------------------- CONTACT FORM------------------------------------->
 		<div id="contact" class="container section">
 			<h2 class="heading">Contact Me</h2>
@@ -261,14 +237,13 @@
 						</div>
 
 						<!-- reCAPTCHA -->
-						<div class="g-recaptcha" data-sitekey="6Led9SkTAAAAAKHa_JtzYJbmfs_lUQhrE0LufdKm"></div>
+						<div class="g-recaptcha" data-sitekey="6Lftrw0UAAAAAJMEE3A0yzZF_6GENXFPUiJwA6Pl"></div>
 
 						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
 						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
 					</form>
 				</div>
 				<div class="col-md-3"></div>
-
 				<!--empty area for form error/success output-->
 				<div class="row">
 					<div class="col-xs-12">
@@ -279,7 +254,7 @@
 		</div>
 		</main>
 		</div>
-		<!--------------------------------------- BEGIN FOOTER ------------------------------------>
+		<!-- begin footer -->
 		<footer>
 			<div class="container">
 				<div class="row">
@@ -293,14 +268,19 @@
 							<span class="fa fa-github fa-2x"></span>
 						</a>
 						<!--LINKEDIN ICON-->
-						<a href="//www.linkedin.com/in/maria-rosado" target="_blank"
+						<a href="//https://www.linkedin.com/in/maria-rosado-bb1608132?trk=hp-identity-name" target="_blank"
 							class="btn btn-social-icon btn-linkedin">
 							<span class="fa fa-linkedin fa-2x"></span>
 						</a>
+						<div class="col-sm-12 text-center">
+							<p>&copy; Copyright Text Here :P</p>
+							<div class="small">
+								<a href="#top"><i class="fa fa-arrow-up"></i> Back to Top</a>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
 		</footer>
 	</body>
+	</head>
 </html>
-
