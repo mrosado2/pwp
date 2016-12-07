50c38131e3447c500159e90be8af3310d305656f
Maria Rosado <mrosado2@cnm.edu>
2016-12-06 22:01:54 -0700

    corrected typo

--- public_html/index.php
+++ public_html/index.php
@@ -66,7 +66,7 @@
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-							<a class="navbar-brand page-scroll">| BlackCat Web Develoment |</a>
+							<a class="navbar-brand page-scroll">| BlackCat Web Development |</a>
 						</div>
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
